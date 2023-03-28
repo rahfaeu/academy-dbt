cee072faf9ac3223904c716b320ff17b76ce0eb8
Rafael dos Santos <rahfaeu@gmail.com>
2023-03-28 23:22:27 +0000

    Criando a tabela dim_sap_adw_address

--- models/marts_analytics/dim_sap_adw_customer.sql
+++ models/marts_analytics/dim_sap_adw_customer.sql
@@ -41,11 +41,7 @@ with
     )
     , creating_customer_sk as (
         select
-            {{ numeric_surrogate_key([
-                'customer_id'
-                , 'customer_name'
-                , 'customer_territory_id'
-            ]) }} as customer_sk
+            {{ numeric_surrogate_key(['customer_id']) }} as customer_sk
             , customer_id
             , customer_name
             , customer_territory_id
