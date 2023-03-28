bd67f7d933a76a1a6d7caaf40496b478e8025be4
Rafael dos Santos <rahfaeu@gmail.com>
2023-03-28 17:35:13 +0000

    Criando a tabela customer em staging

---
+++ models/staging/sap_adw/sales/stg_sap_adw_customer.sql
@@ -0,0 +1,31 @@
+{{ config (
+    materialized = 'table'
+    , partition_by = {
+        'field': 'modified_date'
+        , 'data_type': 'date'
+    }
+    , cluster_by = [
+        'customer_id'
+        , 'customer_person_id'
+        , 'customer_store_id'
+        , 'customer_territory_id'
+    ]
+) }}
+
+/* 
+    Selecionei somente as colunas pertinentes para o desafio.
+    As demais colunas serão comentadas.
+*/
+with
+    source_customer as(
+        select
+            cast(customerid as int) as customer_id
+            , cast(personid as int) as customer_person_id
+            , cast(storeid as int) as customer_store_id
+            , cast(territoryid as int) as customer_territory_id
+            --, rowguid
+            , date(parse_timestamp('%Y-%m-%d %H:%M:%E*S', modifieddate)) as modified_date
+        from {{ source('sap_adw', 'customer') }}
+    )
+select *
+from source_customer
